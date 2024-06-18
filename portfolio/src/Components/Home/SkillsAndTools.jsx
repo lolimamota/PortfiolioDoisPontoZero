71c3f4e1d1380f83710dd8273a92af6d2f015d24
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-06-18 14:58:24 -0300

    criação da array corretamente para empregar o uso de map

--- portfolio/src/Components/Home/SkillsAndTools.jsx
+++ portfolio/src/Components/Home/SkillsAndTools.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import * as S from './StyleHome.jsx';
 
 //------------------ IMPORT IMAGES: SKILLS AND TOOLS ------------------
@@ -21,44 +21,39 @@ function SkillsAndTools() {
   let navigate = useNavigate();
 
   const handleClickMBACK = () => navigate(-1);
-  
-  const images = [
-    <img Title="HTML - Hypertext Markup Language" src={Html} alt="Imagem que remete ao Hypertext Markup Language" />,
-    <img Title="CSS - Cascating Style Sheet" src={Css} alt="Imagem que remete ao Cascating Style Sheet" />,
-    <img Title="JS - JavaScript program language" src={Js} alt="Imagem que remete ao JavaScript" />,
-    <img Title="React - Facebook Library" src={ReactLogo} alt="Imagem da logo do React" />,
-    <img Title="React-Router-DOM - Routes system" src={Rote} alt="Imagem que remete ao React-Router-Dom" />,
-    <img Title="Styled-Components - Cascating Style Sheet on React" src={StyledLogo} alt="Imagem da logo do Styled-Components" />,
-    <img Title="Git, Github - Tools and versioning" src={Git} alt="Imagem que remete a ação de commit" />,
-    <img Title="Cloud Computing - Machines, servers and services stored online" src={Cloud} alt="imagem que remete a computação em nuvem" />
-  ]
 
-  const [text, setText] = useState("")
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  // const images = [
+  //   <img Title="HTML - Hypertext Markup Language" src={Html} alt="Imagem que remete ao Hypertext Markup Language" />,
+  //   <img Title="CSS - Cascating Style Sheet" src={Css} alt="Imagem que remete ao Cascating Style Sheet" />,
+  //   <img Title="JS - JavaScript program language" src={Js} alt="Imagem que remete ao JavaScript" />,
+  //   <img Title="React - Facebook Library" src={ReactLogo} alt="Imagem da logo do React" />,
+  //   <img Title="React-Router-DOM - Routes system" src={Rote} alt="Imagem que remete ao React-Router-Dom" />,
+  //   <img Title="Styled-Components - Cascating Style Sheet on React" src={StyledLogo} alt="Imagem da logo do Styled-Components" />,
+  //   <img Title="Git, Github - Tools and versioning" src={Git} alt="Imagem que remete a ação de commit" />,
+  //   <img Title="Cloud Computing - Machines, servers and services stored online" src={Cloud} alt="imagem que remete a computação em nuvem" />
+  // ]
 
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
+  // const [text, setText] = useState("")
+  // const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
+  // const nextImage = () => {
+  //   setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  // };
+
+  // const prevImage = () => {
+  //   setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  // };
 
   return (
     <S.SectionSkillAndTools>
-        <S.Button onClick={() => handleClickMBACK()}>Back</S.Button>
-        <S.DivSkills>
-          <img src={Wordmap} alt="Mapa com formato de nuvem contendo as palavras, Liderança, Pró ativa, Organizada, Metodologia Ágil, Persistência, Marketing Pessoal, Disciplina, Estudiosa, Curiosa, Facilidade em aprender, Desenvolvimento pessoal, Praticas de Comunicação não violenta, Resolução de problemas, Mentoria de carreira, Palestrante voluntária, Trabalho em equipe, Inglês básico, Interpretação de texto, Solícita e Sincera " />
-        </S.DivSkills>
-        <S.DivCarrousel>
-          <img src={Back} alt="Foto anterior" onClick={nextImage} />
-          <img src={currentImageIndex} alt={Titulo} />
-          <h4>{Title}</h4>
-          <img src={Go} alt="Foto posterior" onClick={prevImage}/>
-        </S.DivCarrousel>
-
+      <S.Button onClick={() => handleClickMBACK()}>Back</S.Button>
+      <S.DivSkills>
+        <img src={Wordmap} alt="Mapa com formato de nuvem contendo as palavras, Liderança, Pró ativa, Organizada, Metodologia Ágil, Persistência, Marketing Pessoal, Disciplina, Estudiosa, Curiosa, Facilidade em aprender, Desenvolvimento pessoal, Praticas de Comunicação não violenta, Resolução de problemas, Mentoria de carreira, Palestrante voluntária, Trabalho em equipe, Inglês básico, Interpretação de texto, Solícita e Sincera " />
+      </S.DivSkills>
+      <S.DivCarrousel>
         
-      </S.SectionSkillAndTools>
+      </S.DivCarrousel>
+    </S.SectionSkillAndTools>
   )
 }
 
