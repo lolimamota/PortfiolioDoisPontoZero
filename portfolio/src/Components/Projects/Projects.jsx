ff85a8729bb3e7707b51255aed7f556806c0b7c8
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-07-09 18:31:02 -0300

    organização de grid de projetos

--- portfolio/src/Components/Projects/Projects.jsx
+++ portfolio/src/Components/Projects/Projects.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import * as S from './StyleProjects.jsx';
+
+import teste from '../Home/Image/css.png'
 
 function Projects() {
   return (
-    <div>Projects</div>
+  <S.SectionProject>
+    <h2>Discover the world on <spam>my Projects</spam></h2>
+    <S.DivGallery>
+      <img src={teste} className='teste1' alt="imagem teste" />
+      <img src={teste} className='teste2' alt="imagem teste" />
+      <img src={teste} className='teste3' alt="imagem teste" />
+      <img src={teste} className='teste4' alt="imagem teste" />
+      <img src={teste} className='teste5' alt="imagem teste" />
+      <img src={teste} className='teste6' alt="imagem teste" />
+      <img src={teste} className='teste8' alt="imagem teste" />
+      <img src={teste} className='teste9' alt="imagem teste" />
+      <img src={teste} className='teste10' alt="imagem teste" />
+    </S.DivGallery>
+  </S.SectionProject>
   )
 }
 
--- portfolio/src/Components/Projects/StyleProjects.jsx
+++ portfolio/src/Components/Projects/StyleProjects.jsx
@@ -1,1 +1,30 @@
 import styled from "styled-components";
+
+export const SectionProject = styled.section`
+    color: white;
+    width: 100%;
+    height: 100vh;
+    background-color: #00040b;
+    text-align: center;
+    padding-top: 2rem;
+    h2{
+        font-size: 1rem;
+    }
+    spam{
+        color: #16E232;
+    }
+`;
+
+export const DivGallery = styled.div`
+    border: solid purple;
+    width: 100%;
+    height: 80vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: repeat(6 , 1fr);
+    grid-template-areas: 
+    " teste1 teste1 teste2 " 
+    " teste1 teste 1 teste3 "
+    " teste4 teste5 teste3 "
+    " teste6 teste5 teste7 ";
+`;
